1762e5ca437349b9ed085ca6b4d1bb4dfad41469
Azha Omerta <azhaomerta29@outlook.com>
2021-01-23 00:34:14 -0600

    Initial Commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$title = "Hello World";
+include ".\\lib\\header.php";
+?>
+
+<h1>Hello World</h1>
+<p>Hello World using PHP</p>
+
+<?php
+include ".\\lib\\footer.php";
+?>
---
+++ lib/footer.php
@@ -0,0 +1,4 @@
+    <hr/>
+    <?php echo date(DATE_RFC2822); ?>
+</body>
+</html>
